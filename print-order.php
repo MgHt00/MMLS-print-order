057b24d81d96af7f39eb022956974df7f36593b1
Mg Htoo <whshwe@gmail.com>
2024-11-20 10:16:28 +0630

    PHP file initialized

---
+++ print-order.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Print Buttons
+ * Description: Adds "Print Invoice" and "Print Shipping" buttons to the WooCommerce order page.
+ * Version: 1.0
+ * Author: Win Htoo Shwe
+ */
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+// Main plugin function
+function wc_print_buttons_enqueue_scripts() {
+    // Enqueue your JavaScript file (to be created later)
+    wp_enqueue_script( 'wc-print-buttons-script', plugin_dir_url( __FILE__ ) . 'js/print-buttons.js', array( 'jquery' ), '1.0', true );
+}
+
+add_action( 'admin_enqueue_scripts', 'wc_print_buttons_enqueue_scripts' );
+
+// Add buttons to the WooCommerce order page
+function wc_print_buttons_add_to_order_page() {
+  global $post;
+
+  // Ensure we're on a WooCommerce order page
+  if ( get_post_type( $post ) !== 'shop_order' ) {
+      return;
+  }
+
+  echo '<div id="wc-print-buttons-sidebar">';
+  echo '<button id="print-invoice" class="button">Print Invoice</button>';
+  echo '<button id="print-shipping" class="button">Print Shipping</button>';
+  echo '</div>';
+}
+
+add_action( 'add_meta_boxes', 'wc_print_buttons_add_to_order_page' );
