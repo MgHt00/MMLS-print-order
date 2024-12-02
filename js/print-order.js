b6fb7cf56b41e94995a34ffdf5aeedf56f0e5fae
Mg Htoo <whshwe@gmail.com>
2024-12-02 12:02:47 +0630

    JS changed var to let

--- js/print-order.js
+++ js/print-order.js
@@ -1,110 +1,108 @@
 jQuery(document).ready(function($) {
-  console.log("jQuery:", $); // This should work
-  console.log('ajaxurl:', ajax_object.ajaxurl); // Log ajaxurl for debugging
+    console.log("jQuery:", $); // This should work
+    console.log('ajaxurl:', ajax_object.ajaxurl); // Log ajaxurl for debugging
 
-// Handle the Print Invoice button click
-$('#print-invoice').click(function(e) {
-    e.preventDefault(); // Prevent default button behavior
-    console.log('Print Invoice clicked');
-    var orderId = $(this).data('order-id'); // Get order ID from data attribute [LE2]
+    // Handle the Print Invoice button click
+    $('#print-invoice').click(function(e) {
+        e.preventDefault(); // Prevent default button behavior
+        console.log('Print Invoice clicked');
+        let orderId = $(this).data('order-id'); // Get order ID from data attribute [LE2]
 
-    $.ajax({
-        url: ajax_object.ajaxurl,
-        method: 'POST',
-        data: {
-            action: 'generate_invoice',
-            order_id: orderId
-        },
-        success: function (response) {
-          console.log("inside sucess:function", response); // Check the full response in the console
-          if (response.success && response.data.invoice) {
-            // Successfully fetched the invoice content
-            var cssUrl = `${ajax_object.plugin_url}/css/print-order-style.css`;
-            console.log("CSS File URL:", cssUrl);
+        $.ajax({
+            url: ajax_object.ajaxurl,
+            method: 'POST',
+            data: {
+                action: 'generate_invoice',
+                order_id: orderId
+            },
+            success: function (response) {
+                console.log("inside success:function", response); // Check the full response in the console
+                if (response.success && response.data.invoice) {
+                    // Successfully fetched the invoice content
+                    let cssUrl = `${ajax_object.plugin_url}/css/print-order-style.css`;
+                    console.log("CSS File URL:", cssUrl);
 
-            var logoUrl = `${ajax_object.plugin_url}/images/logo.png`;
+                    let logoUrl = `${ajax_object.plugin_url}/images/logo.png`;
 
-            var invoiceContent = `
-            <html>
-                <head>
-                    <title>Invoice</title>
-                    <link rel="stylesheet" type="text/css" href="${cssUrl}" media="all">
-                </head>
-                <body>
-                    <div id="invoice-container">
-                        <div id="invoice-header">
-                            <div class="header-content">
-                                <div class="logo-section">
-                                    <img src="${logoUrl}" alt="Company Logo" />
-                                </div>
-                                <div class="address-section">
-                                    <div class="address-header">
-                                        <strong>From:</strong>
+                    let invoiceContent = `
+                    <html>
+                        <head>
+                            <title>Invoice</title>
+                            <link rel="stylesheet" type="text/css" href="${cssUrl}" media="all">
+                        </head>
+                        <body>
+                            <div id="invoice-container">
+                                <div id="invoice-header">
+                                    <div class="header-content">
+                                        <div class="logo-section">
+                                            <img src="${logoUrl}" alt="Myanmar Lifestyle" />
+                                        </div>
+                                        <div class="address-section">
+                                            <div class="address-header">
+                                                <strong>From:</strong>
+                                            </div>
+                                            <div class="address-details">
+                                                Myanmar Lifestyle Fulfillment Center, 35 (first Floor), West Arzarni Street, Yangon, Bahan, 11201, Myanmar
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="address-details">
-                                        Myanmar Lifestyle Fulfillment Center, 35 (first Floor), West Arzarni Street, Yangon, Bahan, 11201, Myanmar
-                                    </div>
                                 </div>
+
+                                ${response.data.invoice}
                             </div>
-                        </div>
+                        </body>
+                    </html>`;
 
-                        ${response.data.invoice}
-                    </div>
-                </body>
-            </html>`;
+                    let printWindow = window.open('', '_blank'); // Open a new print window
+                    printWindow.document.write(invoiceContent); // Write the HTML content
+                    printWindow.document.close(); // Close the document to signal the browser to load it
 
-
-            var printWindow = window.open('', '_blank'); // Open a new print window
-            printWindow.document.write(invoiceContent); // Write the HTML content
-            printWindow.document.close(); // Close the document to signal the browser to load it
-
-            // Wait for the print window to fully load the CSS and content
-            printWindow.onload = function () {
-              console.log("Print window content loaded and styled.");
-              console.log("Stylesheets:", printWindow.document.styleSheets);
-              printWindow.print(); // Trigger the print dialog
-          };
-          
-          } else {
-              alert('Failed to fetch invoice data. Please try again.');
+                    // Wait for the print window to fully load the CSS and content
+                    printWindow.onload = function () {
+                        console.log("Print window content loaded and styled.");
+                        console.log("Stylesheets:", printWindow.document.styleSheets);
+                        printWindow.print(); // Trigger the print dialog
+                    };
+                } else {
+                    alert('Failed to fetch invoice data. Please try again.');
+                }
+            },
+            error: function () {
+                alert('An error occurred. Please try again.');
             }
-        },
-        error: function () {
-          alert('An error occurred. Please try again.');
-        }
+        });
     });
-});
 
-// Handle the Print Shipping button click
-$('#print-shipping').click(function(e) {
-    e.preventDefault(); // Prevent default button behavior
-    console.log('Print Shipping clicked');
-    var orderId = $(this).data('order-id'); // Get order ID
+    // Handle the Print Shipping button click
+    $('#print-shipping').click(function(e) {
+        e.preventDefault(); // Prevent default button behavior
+        console.log('Print Shipping clicked');
+        let orderId = $(this).data('order-id'); // Get order ID
 
-    $.ajax({
-        url: ajax_object.ajaxurl,
-        type: 'POST',
-        data: {
-            action: 'generate_shipping', // Your custom AJAX action
-            order_id: orderId,
-        },
-        success: function(response) {
-            if (response.success) {
-              // Open a new window and display the invoice content                    
-              var printWindow = window.open('', '_blank');
-              printWindow.document.write('<html><head><title>Invoice</title></head><body>'); // Open a full HTML document
-              printWindow.document.write(response.data); // Write invoice HTML to the new window
-              printWindow.document.write('</body></html>'); // Close the HTML document
-              printWindow.document.close();
-              printWindow.print(); // Trigger print dialog
-            } else {
-              console.log('Invoice generation failed: ', response); // Log error response
-              alert('Failed to generate shipping label.');
+        $.ajax({
+            url: ajax_object.ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'generate_shipping', // Your custom AJAX action
+                order_id: orderId,
+            },
+            success: function(response) {
+                if (response.success) {
+                    // Open a new window and display the invoice content                    
+                    let printWindow = window.open('', '_blank');
+                    printWindow.document.write('<html><head><title>Invoice</title></head><body>'); // Open a full HTML document
+                    printWindow.document.write(response.data); // Write invoice HTML to the new window
+                    printWindow.document.write('</body></html>'); // Close the HTML document
+                    printWindow.document.close();
+                    printWindow.print(); // Trigger print dialog
+                } else {
+                    console.log('Invoice generation failed: ', response); // Log error response
+                    alert('Failed to generate shipping label.');
+                }
+            },
+            error: function(error) {
+                console.log('AJAX Request Error:', error);
             }
-        },
-        error: function(error) {
-            console.log('AJAX Request Error:', error);
-        }
+        });
     });
-});
 });
